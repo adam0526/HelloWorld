694d52b004a67cffefec1c93ce56d0f021c5ba24
Adam Liang <KH.Liang@mediatek.com>
2015-06-08 20:21:00 +0800

    Commit--005

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -11,6 +11,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "Hello World 02" << endl;
 	cout << "Hello World 03" << endl;
 	cout << "Hello World 04" << endl;
+	cout << "Hello World 05" << endl;
 	return 0;
 }
 
